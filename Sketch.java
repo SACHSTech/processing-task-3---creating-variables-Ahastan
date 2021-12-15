86f30f3415a9830b82dcb40d107a6d9d2bfe66f5
Ahastan <ahastan.surees23@ycdsbk12.ca>
2021-12-14 23:21:31 -0500

    processing task 3 commit

--- Sketch.java
+++ Sketch.java
@@ -16,20 +16,37 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(66, 135, 245);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Object using random locations
+    
+    float circleX = 200;
+    float circleY = 200;
+    float circleSize = 200;
+    float circleDiameter = 200;
+    float lineX1 = 200;
+    float lineY1 = 200;
+    float lineX2 = random(0, 800);
+    float lineY2 = random(0, 800);
+    
+    // stroke(230, 255, 8);
+    // fill(230, 255, 8);
+    // ellipse(circleX, circleY, circleSize, circleDiameter);
+    
+
+    //Clock
+    stroke(230, 255, 8);
+    fill(230, 255, 8);
+    ellipse(circleX, circleY, circleSize, circleDiameter);
+
+    stroke(66, 135, 245);
+    line(lineX1, lineY1, lineX2, lineY2);
   }
   
   // define other methods down here.
