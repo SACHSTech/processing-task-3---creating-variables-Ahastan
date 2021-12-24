b39792b4eeca4ff46f8a73aa1bab441ed5a078e8
Ahastan <ahastan.surees23@ycdsbk12.ca>
2021-12-24 15:10:47 -0500

    processing task 3 final

--- Sketch.java
+++ Sketch.java
@@ -1,11 +1,19 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  public float rectX = random(0, 300);
+  public float rectY = random(0, 300);
+  public float circleX1 = rectX + 10;
+  public float circleY1 = rectY;
+  public float circleX2 = rectX - 20;
+  public float circleY2 = rectY;
+  public float circleX3 = rectX + 40;
+  public float circleY3 = rectY;
+  public float circleX4 = rectX + 10;
+  public float circleY4 = rectY - 30;
+
+  
   public void settings() {
 	// put your size call here
     size(400, 400);
@@ -17,36 +25,57 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(66, 135, 245);
+    
+    
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-
+    
     // Object using random locations
     
-    float circleX = 200;
-    float circleY = 200;
-    float circleSize = 200;
-    float circleDiameter = 200;
-    float lineX1 = 200;
-    float lineY1 = 200;
-    float lineX2 = random(0, 800);
-    float lineY2 = random(0, 800);
+    //sand
+    stroke(240,230,140);
+    fill(240,230,140);
+    rect(0,height/1.6f, width, height-100);
     
-    // stroke(230, 255, 8);
-    // fill(230, 255, 8);
-    // ellipse(circleX, circleY, circleSize, circleDiameter);
+  //  grass
+    stroke(55, 135, 39);
+    fill(55, 135, 39);
+    bezier(width,height/2, width, height/0.8f, width/0.8f, height/0.8f, width/2, height);
     
-
-    //Clock
+  //  sun
     stroke(230, 255, 8);
     fill(230, 255, 8);
-    ellipse(circleX, circleY, circleSize, circleDiameter);
+    ellipse(0, 0, height/2, width/2);
 
-    stroke(66, 135, 245);
-    line(lineX1, lineY1, lineX2, lineY2);
+    // tree
+    stroke(165,42,42);
+    fill(165,42,42);
+    rect(rectX, rectY, width/20, height/2.7f);
+    
+    stroke(45, 201, 14);
+    fill(45, 201, 14);
+    ellipse(circleX1 , circleY1 , width/8 , height/8);
+    
+    stroke(45, 201, 14);
+    fill(45, 201, 14);
+    ellipse(circleX2, circleY2 , width/8 , width/8);
+    
+    stroke(45, 201, 14);
+    fill(45, 201, 14);
+    ellipse(circleX3, circleY3 , width/8 , height/8);
+    
+    stroke(45, 201, 14);
+    fill(45, 201, 14);
+    ellipse(circleX4, circleY4, width/8, height/8);
+  
+    
+    //declaring variables for tree
+    
+
   }
   
   // define other methods down here.
